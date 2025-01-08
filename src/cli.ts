f64617cc449654ea0a57125606fe1d0332356c7f
lukas-brg <gh.lukas.brg@gmail.com>
2025-01-08 19:20:14 +0100

    Improve CLI

--- src/cli.ts
+++ src/cli.ts
@@ -6,20 +6,63 @@ import path from "path";
 
 import { renderFile } from "./core/index.js";
 
+const isDebugMode = true;
+const debugDefaultInput = "test.md";
+
+function isDirectory(filePath: string) {
+    const hasSep = filePath.endsWith(path.sep);
+    if (hasSep) return true;
+    return path.basename(filePath, "html") === filePath;
+}
+
 program
     .name("xmd")
-    .argument("<file>", "Path to the input Markdown file")
-    .action((file) => {
-        const filePath = path.resolve(file);
+    .argument("[input]", "Path to the input Markdown file")
+    .argument("[output]", "Path to the output file or directory (optional)")
+    .action((input, output) => {
+        if (!input) {
+            if (isDebugMode) {
+                input = debugDefaultInput;
+                console.log("Running in debug mode. Using 'test.md' as input.");
+            } else {
+                program.error("error: missing required argument 'input'");
+            }
+        }
 
-        if (!fs.existsSync(filePath)) {
-            console.error(`Error: File "${file}" does not exist.`);
+        const inputPath = path.resolve(input);
+
+        if (!fs.existsSync(inputPath)) {
+            console.error(`Error: Input file "${inputPath}" does not exist.`);
             process.exit(1);
         }
-        const outputPath = filePath.replace(/\.md$/, ".html");
-        renderFile(filePath, outputPath);
 
-        console.log(`Sucessfully converted '${filePath}' to '${outputPath}'`);
+        if (fs.statSync(inputPath).isDirectory()) {
+            program.error(`Error: input path '${input}' is a directory.`);
+        }
+
+        output = output ?? path.basename(input, ".md") + ".html";
+
+        let outputPath = path.resolve(output);
+
+        console.log(outputPath);
+        if (fs.existsSync(outputPath)) {
+            // If the path exists, it is either a valid directory, or an already existing file
+            if (fs.statSync(outputPath).isDirectory()) {
+                const outputFileName = path.basename(input, ".md") + ".html";
+                outputPath = path.join(path.dirname(outputPath), outputFileName);
+            } else {
+                console.error(`Overwriting file ${outputPath}`);
+            }
+        } else {
+            if (isDirectory(output)) {
+                program.error(`Error: Directory ${output} does not exist.`);
+            }
+        }
+
+        renderFile(inputPath, outputPath);
+        console.log(
+            `Successfully converted '${input}' to '${path.relative(process.cwd(), outputPath)}'`,
+        );
     });
 
 program.parse(process.argv);
