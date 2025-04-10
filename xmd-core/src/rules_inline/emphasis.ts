43802f1181074d27b38f7554940531a33171d87b
lukas <lukas@brg.de>
2025-04-10 17:51:29 +0200

    wip

--- xmd-core/src/rules_inline/emphasis.ts
+++ xmd-core/src/rules_inline/emphasis.ts
@@ -4,52 +4,37 @@ import { InlineToken } from "../token.js";
 import { Dict } from "../util.js";
 import { pairs } from "../util.js";
 import { RuleState } from "../rules.js";
+const chars = new Set(["*", "_", " "]);
+
+function isLeftFlanking(pos: number, line: string): boolean {
+    if (pos + 1 >= line.length) {
+        return false;
+    }
+    return !chars.has(line.charAt(pos + 1));
+}
 
 export const Emphasis: InlineRule = {
     name: "emphasis",
 
     process: (state: ParsingStateInline, ruleState: RuleState) => {
         // prettier-ignore
-        let tokenPositions: Dict<number[]> = {
-            "*": [],
-            "**": [],
-            "_": [],
-            "__": [],
-        };
-
-        // prettier-ignore
 
         let posAstr = state.findIndices(/\*/g);
         let posUnder = state.findIndices(/_/g);
 
         if (!posAstr && !posUnder) return false;
-
-        let stack: string[] = [];
-        for (let i = 0; i < state.line.length; ) {
-            const char = state.charAt(i);
-            if (char == "_" || char == "*") {
+        // *j*text*
+        let stack: [number, string][] = [];
+        for (let i = 0; i < posAstr.length - 1; i++) {
+            const pos = posAstr[i];
+            if (posAstr[i + 1] === pos + 1) {
+                stack.push([pos, "**"]);
+            } else {
+                stack.push([pos, "*"]);
             }
-            i++;
         }
 
         let madeChange = false;
-
-        for (let [tok, positions] of Object.entries(tokenPositions)) {
-            if (positions.length % 2 != 0) continue;
-            const tokLen = tok.length;
-            const tag = tokLen == 2 ? "strong" : "em";
-            for (let [start, end] of pairs(positions)) {
-                state.addInlineToken(
-                    start,
-                    InlineToken.createOpening(tag, start, Emphasis.name, start + tokLen),
-                );
-                state.addInlineToken(
-                    end,
-                    InlineToken.createClosing(tag, end, Emphasis.name, end + tokLen),
-                );
-                madeChange = true;
-            }
-        }
 
         return madeChange;
     },
